2db9501c006221daebc507a13f605937bc304c0c
juliaCCC <105754513+juliaCCC@users.noreply.github.com>
2023-03-22 19:56:03 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function setup() {
+    canvas = createCanvas(700,500);
+    canvas.position(400,200);
+    canvas.background(179, 182, 235);
+}
+ function draw() {
+    rect(546, 299, 125, 49);
+    circle(356, 298, 152)
+    triangle(345, 345, 655, 325, 671, 617)
+ }
